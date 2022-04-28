906a7b27c73bfd31943442ccff1ba4debc408022
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-04-28 12:45:32 -0500

    adding more requirements

--- models/Thought.js
+++ models/Thought.js
@@ -1,17 +1,25 @@
-const { Schema, model } = require('mongoose');
-const ReactionSchema = require('./Reaction');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+const User = require('./User');
 
-ThoughtSchema = new Schema({
+const ThoughtSchema = new Schema({
     thoughtText: {
         type: String,
         required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
     },
     user: {
         type: Schema.Types.ObjectId,
         ref: 'User',
         required: true
     },
-    reactions: [ReactionSchema]
+    reactions: [ReactionSchema],
 },
 {
     toJSON: {
@@ -21,6 +29,41 @@ ThoughtSchema = new Schema({
     id: false
 }
 );
+
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId
+    },
+    reactionBody: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true 
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+},
+{
+    toJSON: {
+        getters: true,
+        virtuals: true
+    },
+    id: false
+}
+);
+
+ThoughtSchema.virtual('username').get(function () {
+    return this.reaction.length;
+});
+
+module.exports = ReactionSchema;
 
 const Thought = model('Thought', ThoughtSchema);
 
